48387a454db4a60b9888118f7c902147773df2f6
Soheab_ <33902984+Soheab@users.noreply.github.com>
2024-02-22 23:27:22 +0100

    Dynamically get version, author and copyright

--- docs/conf.py
+++ docs/conf.py
@@ -6,10 +6,29 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'discord-py-paginators'
-copyright = '2023, Soheab_'
-author = 'Soheab_'
-release = '0.0.1'
+from datetime import date
+import re
+
+
+project = "discord-py-paginators"
+
+# source:
+# https://github.com/Rapptz/discord.py/blob/61eddfcb189f11a293011d43b09fe4ec52641dd2/docs/conf.py#L95C1-L100C18
+version = "0.0.0"
+author = "Unknown Author"
+try:
+    with open("../discord/ext/paginators/__init__.py") as f:
+        read = f.read()
+        version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', read, re.MULTILINE).group(1)  # type: ignore
+        author = re.search(r'^__author__\s*=\s*[\'"]([^\'"]*)[\'"]', read, re.MULTILINE).group(1)  # type: ignore
+except Exception:
+    pass
+
+release = version
+author = author
+
+current_year = date.today().year
+copyright = f"{current_year}, {author}"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -25,23 +44,23 @@ sys.path.insert(0, os.path.abspath(os.path.join("..", "..")))
 
 
 extensions = [
-    'sphinx.ext.viewcode',  # https://www.sphinx-doc.org/en/master/usage/extensions/viewcode.html
-    'sphinx.ext.autodoc',  # https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html
-    'sphinx.ext.napoleon',  # https://www.sphinx-doc.org/en/master/usage/extensions/napoleon.html
-    'sphinx_autodoc_typehints',  # https://github.com/tox-dev/sphinx-autodoc-typehints
+    "sphinx.ext.viewcode",  # https://www.sphinx-doc.org/en/master/usage/extensions/viewcode.html
+    "sphinx.ext.autodoc",  # https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html
+    "sphinx.ext.napoleon",  # https://www.sphinx-doc.org/en/master/usage/extensions/napoleon.html
+    "sphinx_autodoc_typehints",  # https://github.com/tox-dev/sphinx-autodoc-typehints
     "sphinx.ext.intersphinx",  # https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html
-    'sphinx_toolbox.more_autodoc.typevars',  # https://sphinx-toolbox.readthedocs.io/en/latest/extensions/more_autodoc/typevars.html
+    "sphinx_toolbox.more_autodoc.typevars",  # https://sphinx-toolbox.readthedocs.io/en/latest/extensions/more_autodoc/typevars.html
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
-html_static_path = ['_static']
+html_theme = "furo"
+html_static_path = ["_static"]
 
 
 # autodoc
@@ -63,13 +82,13 @@ napoleon_use_rtype = False
 # sphinx_autodoc_typehints
 always_document_param_types = True
 typehints_document_rtype = False
-typehints_defaults = 'braces'
+typehints_defaults = "braces"
 simplify_optional_unions = False
 
 # sphinx_toolbox.more_autodoc.typevars
 all_typevars = True
 
-# intersphinx 
+# intersphinx
 # https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
@@ -88,7 +107,6 @@ nitpick_ignore = [
     ("py:class", "ButtonPaginator[Any]"),
     ("py:class", "typing.Unpack"),
     ("py:class", "discord.ext.paginators.button_paginator.PageSwitcherAndStopButtonView"),
-
     # ????? idk about these, these are from discord.py
     # fixes:
     # <unknown>:1: WARNING: py:data reference target not found: typing.Union`[:py:class:`~discord.emoji.Emoji
